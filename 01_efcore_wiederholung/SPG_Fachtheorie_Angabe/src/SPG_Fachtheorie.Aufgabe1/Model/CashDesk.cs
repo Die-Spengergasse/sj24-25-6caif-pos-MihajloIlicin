1e3ab48adac17da83d89eda793aa6cb66ce9cd3b
Mihajlo Ilicin <ilicinmihajlo@gmail.com>
2025-03-20 16:20:29 +0100

    exercise_01_efcore

---
+++ 01_efcore_wiederholung/SPG_Fachtheorie_Angabe/src/SPG_Fachtheorie.Aufgabe1/Model/CashDesk.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SPG_Fachtheorie.Aufgabe1.Model
+{
+    public class CashDesk
+    {
+        protected CashDesk() { }
+
+        public CashDesk(int number)
+        {
+            Number = number;
+        }
+
+        [Key]
+        public int Number { get; set; }
+    }
+}
